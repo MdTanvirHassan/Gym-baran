bb9e424cbe1eed5b99c862350a1ae069bab37bb9
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-02-16 18:30:44 +0600

    Gym-baran
    Web design with nextjs and tailwind css.

---
+++ components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-[uri("../public/assets/bg.pbg")] h-screen w-full py-44'>
+        <div className="">hero</div>
+    </div>
+  )
+}
+
+export default Hero
