780ecc854f0cc8d463bdaa7a2ef1b7e6d4f3a3af
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-02-17 01:08:29 +0600

    update navbar, add hero component.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
+import { FaChevronCircleRight } from "react-icons/fa";
+//import { BiCaretRightCircle } from "react-icons/bi";
+import { IoMdArrowDroprightCircle } from "react-icons/io";
+import Img from "../public/assets/image77.png";
+import Brands from "../public/assets/brands.png";
 
 const Hero = () => {
   return (
-    <div className='bg-[uri("../public/assets/bg.pbg")] h-screen w-full py-44'>
-        <div className="">hero</div>
-    </div>
-  )
-}
+    <div id="hero" className="md:max-w-[1279px] md:max-h-[582px] m-auto">
+      <section className="">
+        <div className=" m-auto grid py-24 md:grid-cols-2 gap-2 items-center justify-center px-6 md:px-10">
 
-export default Hero
+          <div className="mb-4 md:mb-0 items-center md:ml-[81px]">
+            <h1 className="text-[#262524] text-4xl md:text-6xl mb-1 md:w-[568px] h-[146px]  font-bold tracking-widest ">
+              Healthy in side <br />
+              <span className="text-[#8382EB]">fresh </span>out side
+            </h1>
+            <p className="mb-8 text-[#262524] tracking-wider text-[16px] text-justify leading-7 font-normal">
+              Exercise is a very important need for our body. Health and fitness
+              will be obtained if you can do regular exercise and run a healthy
+              routine.
+            </p>
+            <div className="flex py-4">
+              <button className="inline-flex text-white text-start bg-[#264373] border-0 py-2.5 px-4 focus:outline-none hover:bg-indigo-900 rounded-md shadow-lg shadow-gray-400">
+                <span>Get started</span>{" "}
+                <FaChevronCircleRight className="text-white text-xl ml-8 md:ml-16" />
+              </button>
+              <button className="ml-4 inline-flex text-[#264373] bg-white border-0 py-2.5 px-4 focus:outline-none hover:bg-gray-200 rounded-md  shadow-lg shadow-gray-400 space-x-2">
+                <IoMdArrowDroprightCircle className="text-2xl" />
+                <span className="">Learn more</span>
+              </button>
+            </div>
+
+            <div className="brands my-4">
+              <div>Brands:</div>
+              <div className=" my-4">
+                <Image
+                  className="relative z-10 object-cover object-center rounded"
+                  alt="brands"
+                  src={Brands}
+                  width={463}
+                  height={64}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="m-auto">
+            <Image
+              className="relative z-10 object-cover object-center rounded py-8"
+              alt="hero"
+              src={Img}
+              width={358}
+              height={580}
+            />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,135 +31,121 @@ const Navbar = () => {
   }, []);
 
   return (
-    <>
-      <div
-        style={{ backgroundColor: `${navBg}` }}
-        className={
-          shadow
-            ? "fixed w-full  h-[90px] shadow-md shadow-gray-400 z-[100] ease-in-out duration-300 m-auto"
-            : "fixed w-full h-[90px] z-[100] m-auto"
-        }>
-        <div className="flex justify-between items-center w-[1246px] h-[50px] px-1 2xl:px-16 md:mt-[42px] m-auto">
-          <Link href="/">
-            <div className="mx-20 flex space-x-2"
-                style={{ backgroundColor: `${shadow}` }}>
-              <Image
-                src={NavLogo}
-                alt="nav-logo"
-                width="141"
-                height="38"
-                className="cursor-pointer m-auto mt-6"
-              />
-              
-            </div>
-          </Link>
-          <div>
-            <ul
-              style={{ color: `${linkColor}` }}
-              className="hidden md:flex space-x-8 mx-10">
-              <li className="ml-7 text-bold  ">
-                <Link href="/">Home</Link>
-              </li>
-              <li className="ml-7 text-bold  ">
-                <Link href="/#program">Program</Link>
-              </li>
-              <li className="ml-7 text-bold  ">
-                <Link href="/#blog">Blog</Link>
-              </li>
-              <li className="ml-7 text-bold  ">
-                <Link href="/#about">About us</Link>
-              </li>
-
-              <li className="ml-7 text-bold  ">
-                <Link href="/#login">
-                    <button type="button" className="bg-[#264373] text-white w-[114px] h-[50px] rounded-xl -mt-5">Log in</button>
-                    </Link>
-              </li>
-            </ul>
-            {/* Hamburger Icon */}
-            <div
-              style={{ color: `${linkColor}` }}
-              onClick={handleNav}
-              className="md:hidden rounded-full bg-gray-600 p-2">
-              <AiOutlineMenu size={25} className="text-white" />
-            </div>
+    <div
+      style={{ backgroundColor: `${navBg}` }}
+      className={
+        shadow
+          ? "fixed w-full h-20 md:h-[75px] shadow-lg shadow-gray-700 z-[100] ease-in-out duration-300"
+          : "fixed w-full h-20 z-[100]"
+      }>
+      <div className="flex justify-between items-center w-full h-full md:w-[1246px] md:h-[50px] m-auto md:mt-[22px]">
+        <Link href="/">
+          <div className="">
+            <Image
+              src={NavLogo}
+              alt="/"
+              width="141"
+              height="38"
+              className="cursor-pointer  m-auto mt-6"
+            />
           </div>
-        </div>
-
-        {/* Mobile Menu */}
-        {/* Overlay */}
-        <div
-          className={
-            nav
-              ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70"
-              : ""
-          }>
-          {/* Side Drawer Menu */}
+        </Link>
+        <div>
+          <ul style={{ color: `${linkColor}` }} className="hidden md:flex">
+            
+            <li className="ml-7 text-bold">
+              <Link href="/">Home</Link>
+            </li>
+            <li className="ml-7 text-bold">
+              <Link href="/#program">Program</Link>
+            </li>
+            <li className="ml-7 text-bold">
+              <Link href="/#blog">Blog</Link>
+            </li>
+            <li className="ml-7 text-bold">
+              <Link href="/#about">About us</Link>
+            </li>
+            
+            <li className="ml-7 text-bold">
+              <Link href="/#login">
+              <button type="button" className="bg-[#264373] text-white w-[114px] h-[50px] rounded-xl -mt-5">Log in</button>
+              </Link>
+            </li>
+          </ul>
+          {/* Hamburger Icon */}
           <div
-            className={
-              nav
-                ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
-                : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
-            }>
-            <div>
-              <div className="flex w-full items-center justify-between">
-                <Link href="/">
-                  <div className={
-                  shadow
-                    ? "cursor-pointer bg-gray-200 px-1 py-2 rounded-md"
-                    : "cursor-pointer"
-                }>
-                    <Image src={NavLogo} width="141" height="38" alt="/" />
-                  </div>
-                </Link>
-                <div
-                  onClick={handleNav}
-                  className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer ">
-                  <AiOutlineClose className="text-black" />
-                </div>
-              </div>
-              <div className="border-b border-gray-300 my-4">
-                <p className="w-[85%] md:w-[90%] py-4 text-gray-500">
-                  Lorem ipsum dolor sit amet consectetur.
-                </p>
-              </div>
-            </div>
-            <div className="py-4 flex flex-col">
-              <ul className="uppercase">
-                <Link href="/">
-                  <li onClick={() => setNav(false)} className="py-4 text-sm">
-                    Home
-                  </li>
-                </Link>
-                <Link href="/#program">
-                  <li onClick={() => setNav(false)} className="py-4 text-sm">
-                    Program
-                  </li>
-                </Link>
-                <Link href="/#blog">
-                  <li onClick={() => setNav(false)} className="py-4 text-sm">
-                    Blog
-                  </li>
-                </Link>
-                <Link href="/#about">
-                  <li onClick={() => setNav(false)} className="py-4 text-sm">
-                    About us
-                  </li>
-                </Link>
-
-                <Link href="/#login">
-                  <li onClick={() => setNav(false)} className="py-4 text-sm">
-                    Log in
-                  </li>
-                </Link>
-                
-              </ul>
-              <div className="pt-32"></div>
-            </div>
+            style={{ color: `${linkColor}` }}
+            onClick={handleNav}
+            className="md:hidden px-4">
+            <AiOutlineMenu size={25} className="text-black" />
           </div>
         </div>
       </div>
-    </>
+
+      {/* Mobile Menu */}
+      {/* Overlay */}
+      <div
+        className={
+          nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""
+        }>
+        {/* Side Drawer Menu */}
+        <div
+          className={
+            nav
+              ? " fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-5 ease-in duration-500"
+              : "fixed left-[-150%] top-0 p-5 ease-in duration-500"
+          }>
+          <div>
+            <div className="flex flex-row w-full items-center justify-between">
+              <Link href="/">
+                <>
+                  <Image src={NavLogo} width="275" height="75" alt="/" className=""/>
+                </>
+              </Link>
+              <div
+                onClick={handleNav}
+                className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer ">
+                <AiOutlineClose className="text-black" />
+              </div>
+            </div>
+            <div className="border-b border-gray-300 my-2">
+              
+            </div>
+          </div>
+          <div className="py-2 flex flex-col">
+            <ul className="uppercase">
+              <Link href="/">
+                <li onClick={() => setNav(false)} className="py-2 text-sm">
+                  Home
+                </li>
+              </Link>
+              <Link href="/#program">
+                <li onClick={() => setNav(false)} className="py-2 text-sm">
+                  Program
+                </li>
+              </Link>
+              <Link href="/#blog">
+                <li onClick={() => setNav(false)} className="py-2 text-sm">
+                  Blog
+                </li>
+              </Link>
+              <Link href="/#about">
+                <li onClick={() => setNav(false)} className="py-2 text-sm">
+                  About us
+                </li>
+              </Link>
+             
+              <Link href="/#login">
+                <li onClick={() => setNav(false)} className="py-2 text-sm">
+                  Log in
+                </li>
+              </Link>
+            </ul>
+            
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
