a4edcf44c1d7a0866c4cb87713755842ce927e6b
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-02-17 01:58:12 +0600

    demo of hero.

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -3,39 +3,37 @@ import React from "react";
 import { FaChevronCircleRight } from "react-icons/fa";
 //import { BiCaretRightCircle } from "react-icons/bi";
 import { IoMdArrowDroprightCircle } from "react-icons/io";
-import Img from "../public/assets/image77.png";
+import Img from "../public/assets/hero.png";
 import Brands from "../public/assets/brands.png";
 
 const Hero = () => {
   return (
-    <div id="hero" className="md:max-w-[1279px] md:max-h-[582px] m-auto">
-      <section className="">
-        <div className=" m-auto grid py-24 md:grid-cols-2 gap-2 items-center justify-center px-6 md:px-10">
+    <div id="hero" className=" w-full md:max-w-[1279px] md:max-h-[582px] m-auto">
+      <section className="py-10">
+        <div className=" m-auto grid py-24 md:grid-cols-2 gap-0 items-center justify-center px-6 md:px-0">
 
-          <div className="mb-4 md:mb-0 items-center md:ml-[81px]">
-            <h1 className="text-[#262524] text-4xl md:text-6xl mb-1 md:w-[568px] h-[146px]  font-bold tracking-widest ">
+          <div className="mb-2 md:mb-0 items-center md:ml-[51px]">
+            <h1 className="text-[#262524] text-4xl md:text-6xl mb-1 md:w-[568px] md:h-[146px]  font-bold tracking-wider ">
               Healthy in side <br />
               <span className="text-[#8382EB]">fresh </span>out side
             </h1>
-            <p className="mb-8 text-[#262524] tracking-wider text-[16px] text-justify leading-7 font-normal">
-              Exercise is a very important need for our body. Health and fitness
-              will be obtained if you can do regular exercise and run a healthy
-              routine.
+            <p className="my-6 text-[#262524] md:w-[492px] md:h-[84px] tracking-wider text-[16px] text-justify leading-7 font-normal opacity-60">
+              Exercise is a very important need for our body. Health and fitness will be obtained if you can do regular exercise and run a healthy routine.
             </p>
             <div className="flex py-4">
               <button className="inline-flex text-white text-start bg-[#264373] border-0 py-2.5 px-4 focus:outline-none hover:bg-indigo-900 rounded-md shadow-lg shadow-gray-400">
                 <span>Get started</span>{" "}
-                <FaChevronCircleRight className="text-white text-xl ml-8 md:ml-16" />
+                <FaChevronCircleRight className="text-white text-xl ml-14 md:ml-16" />
               </button>
-              <button className="ml-4 inline-flex text-[#264373] bg-white border-0 py-2.5 px-4 focus:outline-none hover:bg-gray-200 rounded-md  shadow-lg shadow-gray-400 space-x-2">
+              <button className="ml-4 inline-flex text-[#264373] bg-white border-0 py-2.5 px-2 md:px-4 focus:outline-none hover:bg-gray-200 rounded-md  shadow-lg shadow-gray-400 space-x-1 md:space-x-2">
                 <IoMdArrowDroprightCircle className="text-2xl" />
                 <span className="">Learn more</span>
               </button>
             </div>
 
-            <div className="brands my-4">
+            <div className="brands mt-6">
               <div>Brands:</div>
-              <div className=" my-4">
+              <div className=" mb-2">
                 <Image
                   className="relative z-10 object-cover object-center rounded"
                   alt="brands"
@@ -46,12 +44,12 @@ const Hero = () => {
               </div>
             </div>
           </div>
-          <div className="m-auto">
+          <div className="m-auto ">
             <Image
-              className="relative z-10 object-cover object-center rounded py-8"
+              className="relative z-10 object-cover object-center rounded md:ml-15"
               alt="hero"
               src={Img}
-              width={358}
+              width={584}
               height={580}
             />
           </div>
