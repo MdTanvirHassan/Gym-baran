714b90ec09782246060c1d01e748326fd3c701ad
Tanvir Hassan <Tanvirtamim72@gmail.com>
2023-02-17 07:16:47 +0600

    Add Workout component.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,25 +7,23 @@ const Footer = () => {
     <div className="pt-8">
       <footer className="bg-[#1C3764] text-white">
         <div className="container px-5 py-16 mx-auto flex  lg:items-start md:flex-row md:flex-nowrap flex-wrap flex-col">
-
-          <div className="w-64 md:mx-0 mx-auto text-left">
+          <div className="w-64 mx-4 md:mx-auto text-left">
             <a className=" justify-start text-white">
               <Image
                 src={Logo}
                 alt="footer-logo"
-                className="-mt-9"
+                className="-mt-9 -ml-12"
                 width={141}
                 height={38}
               />
               <p className=" text-sm text-[#FFFFFF] opacity-50 -mt-8">
-              We take care of your health with regular and fun exercise
-            </p>
+                We take care of your health with regular and fun exercise
+              </p>
             </a>
-            
           </div>
           <div className="flex-grow flex flex-wrap md:pl-20 -mb-10 md:mt-0 mt-10 md:text-left text-start">
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="font-bold text-white text-sm tracking-widest text-sm mb-3">
+              <h2 className="font-bold text-white text-sm tracking-widest mb-3">
                 Program
               </h2>
               <nav className="list-none mb-10">
@@ -52,7 +50,7 @@ const Footer = () => {
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="font-bold text-white text-sm tracking-widest text-sm mb-3">
+              <h2 className="font-bold text-white text-sm tracking-widest mb-3">
                 Blog
               </h2>
               <nav className="list-none mb-10">
@@ -66,11 +64,10 @@ const Footer = () => {
                     Daily workout
                   </a>
                 </li>
-                
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="font-bold text-white text-sm tracking-widest text-sm mb-3">
+              <h2 className="font-bold text-white text-sm tracking-widest mb-3">
                 About Us
               </h2>
               <nav className="list-none mb-10">
@@ -89,11 +86,10 @@ const Footer = () => {
                     Address
                   </a>
                 </li>
-                
               </nav>
             </div>
             <div className="lg:w-1/4 md:w-1/2 w-full px-4">
-              <h2 className="font-bold text-white text-sm tracking-widest text-sm mb-3">
+              <h2 className="font-bold text-white text-sm tracking-widest mb-3">
                 Blog
               </h2>
               <nav className="list-none mb-10">
@@ -107,7 +103,6 @@ const Footer = () => {
                     Daily workout
                   </a>
                 </li>
-                
               </nav>
             </div>
           </div>
